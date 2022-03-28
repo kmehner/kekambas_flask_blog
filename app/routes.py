79edf7d0dd89214f7e07fa5606b7ed7d590188e5
Brian <bstanton773@gmail.com>
2022-03-28 14:45:17 -0500

    Return an html template

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,37 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World!!'
+    title = 'Home'
+    user = {'id': 1, 'username': 'bstanton', 'email': 'brians@codingtemple.com'}
+    posts = [
+        {
+            'id': 1,
+            'title': 'March Madness',
+            'body': 'The NCAA tournament never seems to disappoint!',
+            'author': 'cbbfan'
+        },
+        {
+            'id': 2,
+            'title': 'Oscars',
+            'body': 'I think that Coda might win Best Picture this year. I cried the whole time.',
+            'author': 'BuffMovieBuff123'
+        },
+        {
+            'id': 3,
+            'title': 'Python',
+            'body': 'Python is by far my favorite programming language. It is so neat!',
+            'author': 'code_is_life'
+        },
+        {
+            'id': 4,
+            'title': 'Flask',
+            'body': 'My favorite microframework! And this is only the beginning.',
+            'author': 'CodingTemple4L'
+        }
+    ]
+    return render_template('index.html', current_user=user, title=title, posts=posts)
 
 
 @app.route('/test')
