9043a96d7b9f18c435a3aeeed880e0752e305a7b
Katelyn Mehner <kmehner@gmail.com>
2022-03-29 11:18:32 -0600

    Tuesday Morning

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import Config 
 # We moved the instance of flask from the run.py file to the app file 
 # Also need to say from app import app in run.py file now to link it
 
@@ -7,8 +8,15 @@ app = Flask(__name__)
 # which is set to the name of the module in which it is used
 # Hover over flask - tells us that template folder is "templates"
 
+# secret key (do not hard code this in but okay for now)
+app.config.from_object(Config)
+
+# from app import forms
+
 from app import routes
 # routes must go at the bottom (specific to flask)
 # from app import routes links routes to the app
+
+
 
 
