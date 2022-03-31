b010f48f8e5fb034e5f5df21fcc613cb44172adb
Katelyn Mehner <kmehner@gmail.com>
2022-03-31 13:17:07 -0600

    Update for Heroku

--- app/blueprints/blog/routes.py
+++ app/blueprints/blog/routes.py
@@ -40,10 +40,42 @@ def single_post(post_id):
     title = post.title
     return render_template('post_detail.html', title=title, post=post)
 
-
 @blog.route('/search-posts', methods=['GET', 'POST'])
 def search_posts():
     title = 'Search'
     form = SearchForm()
     posts = []
+    if form.validate_on_submit():
+        term = form.search.data
+        posts = Post.query.filter( (Post.title.ilike(f'%{term}%')) | (Post.body.ilike(f'%{term}%')) ).all()
     return render_template('search_posts.html', title=title, posts=posts, form=form)
+
+# Edit a Single Post by ID
+@blog.route('/edit-posts/<post_id>', methods=["GET", "POST"])
+@login_required
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        flash('You do not have edit access to this post.', 'danger')
+        return redirect(url_for('blog.my_posts'))
+    title = f"Edit {post.title}"
+    form = PostForm()
+    if form.validate_on_submit():
+        post.update(**form.data)
+        flash(f'{post.title} has been updated', 'warning')
+        return redirect(url_for('blog.my_posts'))
+        
+    return render_template('post_edit.html', title=title, post=post, form=form)
+
+
+
+@blog.route('/delete-posts/<post_id>')
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        flash('You do not have delete access to this post', 'danger')
+    else:
+        post.delete()
+        flash(f'{post.title} has been deleted.', 'secondary')
+    return redirect(url_for('blog.my_posts'))
