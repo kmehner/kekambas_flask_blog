8ce942c5753533a99c841e8f99e29b235ad4f78e
Brian <bstanton773@gmail.com>
2022-04-13 12:02:54 -0500

    Create a post route for api

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,11 +1,11 @@
 from . import api
-from flask import jsonify
+from flask import jsonify, request
 from app.blueprints.blog.models import Post
+
 
 @api.route('/')
 def index():
     return jsonify({'test': 'This is a test'})
-
 
 
 # Get all posts
@@ -15,9 +15,27 @@ def get_posts():
     return jsonify([p.to_dict() for p in posts])
 
 
-
 # Get a single post by id
 @api.route('/posts/<int:post_id>')
 def get_single_post(post_id):
     post = Post.query.get_or_404(post_id)
     return jsonify(post.to_dict())
+
+
+# Create a post
+@api.route('/posts/create', methods=['POST'])
+def create_post():
+    if not request.is_json:
+        return jsonify({'error': 'Your request content-type must be application/json'}), 400
+    # Get data from request body
+    data = request.json
+    # Check to make sure all required fields are present
+    for field in ['title', 'body']:
+        if field not in data:
+            # if not return a 400 response with error
+            return jsonify({'error': f'{field} must be in request body'}), 400
+    # Get fields from data dict
+    title = data['title']
+    body = data['body']
+    new_post = Post(title=title, body=body, user_id=1)
+    return jsonify(new_post.to_dict())
